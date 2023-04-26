8c623ad56102c4f5d16ff4f8dd5a5fe044bece6c
Hperigee <carrot060606@gmail.com>
2023-04-26 16:24:31 +0900

    analysis, convert 개발 필요. 나머지 완료

---
+++ analysis.py
@@ -0,0 +1,5 @@
+import librosa
+def analysis(filename):
+    song_directory = '.\\temp\\' + filename + '\\' + 'vocals.wav'
+    waveform, sr = librosa.load(song_directory)
+    pass
---
+++ fileinput.py
@@ -0,0 +1,56 @@
+import librosa
+import os
+import analysis
+
+from spleeter.separator import Separator
+
+
+class SoundFormInfo:
+    def __init__(self, filename):
+        self.filename = filename
+        self.notes=None #시간에 따른 음높이
+        self.express=None #시간에 따른 표현도
+        self.maxnote=None  # 최고음
+        self.soundrange=None #음역 비중
+        self.beatness=None  #박자감
+        self.breath=None # 호흡 지수
+        self.health=None #체력 지수
+
+
+
+
+
+def _converter():
+    pass
+
+def _filename_fetch(directory):
+    assert isinstance(directory, str)
+    ind = directory.rfind('\\')
+    return directory[ind + 1:-4]
+
+def _seperate(directory):
+    separator = Separator('spleeter:2stems')
+    separator.separate_to_file(directory, '.\\temp')
+    return
+
+def _remove_vocal_file(filename):
+    remove_directory = '.\\temp\\' + filename + '\\' + 'vocals.wav'
+    os.remove(remove_directory)
+    return
+
+def inputfile(directory):
+
+    _seperate(directory)    #음원 분리
+
+    filename=_filename_fetch(directory)
+    analysis(filename)      #보컬 정보
+
+    _remove_vocal_file(filename)
+    return
+
+
+
+
+
+if __name__=='__main__':
+    safd=inputfile('.\\THORNAPPLE-Blue_Spring.mp3')
