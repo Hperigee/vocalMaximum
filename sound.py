5974179e423f011bea26f487c9feb30489351452
Syeosle <syeosleyt@gmail.com>
2023-04-26 12:06:36 +0900

    reformated

--- sound.py
+++ sound.py
@@ -1,19 +1,25 @@
 import librosa
 import spleeter
 import pickle
+
+
 class SoundFormInfo:
-    def __init__(self,filename,waveform,sr):
-        self.filename=filename
-        self._waveform=_waveform
-        self._sr=_sr
+    def __init__(self, filename, waveform, sr):
+        self.filename = filename
+        self._waveform = waveform
+        self._sr = sr
+
     def analysis(self):
         pass
+
+
 def inputfile(toinput):
     filename = toinput
     y, sr = librosa.load(filename)
-    song=SoundFormInfo(filename,y,sr)
+    song = SoundFormInfo(filename, y, sr)
     return song
 
+
 def seperate(song):
-    songname=song.filename
+    songname = song.filename
     pass
