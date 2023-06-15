044bb765b740b635110d81738204091fa55f3651
Hperigee <carrot060606@gmail.com>
2023-06-16 03:37:28 +0900

    해치웠나

---
+++ VocalMaximum.bat
@@ -0,0 +1,29 @@
+@echo off
+
+REM Step 2: Check if a virtual environment exists
+if not exist venv\Scripts\activate.bat (
+    echo Creating virtual environment...
+    REM Step 3: Create virtual environment
+    python -m venv venv
+    echo Virtual environment created.
+)
+
+REM Step 3: Activate virtual environment and install requirements
+echo Activating virtual environment...
+call venv\Scripts\activate
+echo Installing requirements...
+pip install --no-cache-dir --no-deps -r requirements.txt
+echo Requirements installed.
+
+REM Step 4: Run Python file using the virtual environment
+echo Running Python file...
+python Interface.py
+
+REM Cleanup: Deactivate virtual environment
+echo Deactivating virtual environment...
+deactivate
+echo Virtual environment deactivated.
+
+REM End of batch file
+echo Batch file execution completed.
+pause
